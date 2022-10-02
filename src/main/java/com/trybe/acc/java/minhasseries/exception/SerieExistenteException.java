786c9b6f79d8e6fbf78b89cd982faa416f2e1f00
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-10-02 18:18:09 -0300

    [Priscila] Minhas Series

---
+++ src/main/java/com/trybe/acc/java/minhasseries/exception/SerieExistenteException.java
@@ -0,0 +1,12 @@
+package com.trybe.acc.java.minhasseries.exception;
+
+/**
+ * Classe: SerieExistenteException.
+ */
+
+@SuppressWarnings("serial")
+public class SerieExistenteException extends RuntimeException {
+  public SerieExistenteException(String message) {
+    super(message);
+  }
+}
