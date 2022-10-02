517c18e5a94019b9571015230e44140be5d77990
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-10-02 19:14:33 -0300

    Implementação - Model Serie

--- src/main/java/com/trybe/acc/java/minhasseries/model/Serie.java
+++ src/main/java/com/trybe/acc/java/minhasseries/model/Serie.java
@@ -1,9 +1,55 @@
 package com.trybe.acc.java.minhasseries.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ * Entity: Serie.
+ */
 
 @Entity
 public class Serie {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String nome;
 
+  public Serie(String nome) {
+    this.nome = nome;
+  }
 
+  @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+  private List<Episodio> episodios = new ArrayList<>();
+
+  public Serie() {}
+
+  /**
+   * Métodos.
+   */
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Episodio> getEpisodios() {
+    return episodios;
+  }
+
+  public void adicionarEpisodio(Episodio episodio) {
+    this.episodios.add(episodio);
+  }
 }
