8a5925a643408a1a43082c616111670e65439636
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-10-02 22:57:27 -0300

    Correção - Checkstyle.

--- src/main/java/com/trybe/acc/java/minhasseries/MinhasSeries.java
+++ src/main/java/com/trybe/acc/java/minhasseries/MinhasSeries.java
@@ -3,6 +3,10 @@ package com.trybe.acc.java.minhasseries;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe: Main.
+ */
+
 @SpringBootApplication
 public class MinhasSeries {
   public static void main(String[] args) {
